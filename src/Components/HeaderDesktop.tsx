68c4becb0fff09a5a24a3bf3d14d244432fd5119
dazzlemon <daanill13470@gmail.com>
2022-01-02 00:07:57 +0200

    fixed bug & cleaned up

--- src/Components/HeaderDesktop.tsx
+++ src/Components/HeaderDesktop.tsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import HeaderDesktopView from '../PureComponents/HeaderDesktopView';
 import {
-	ApolloError,
 	gql,
 	QueryResult
 } from '@apollo/client';
@@ -10,39 +9,17 @@ import {
 // everything I found was for functional react
 import { Query } from '@apollo/client/react/components';
 
-const getCircularReplacer = () => {
-  const seen = new WeakSet();
-  return (key: any, value: any) => {
-    if (typeof value === "object" && value !== null) {
-      if (seen.has(value)) {
-        return;
-      }
-      seen.add(value);
-    }
-    return value;
-  };
-};
-
 const categoryNamesQueryToView = (result: QueryResult<{
 	categories: CategoryName[]
 }>) => {
-	console.log('render');
 	const { loading, error, data } = result;
 	const status = loading ? 'loading'
 	             : error   ? 'error'
 	             : null;
-	// with below it wont update
 	if (status) {
 		return <HeaderDesktopView status={status} />;
 	}
-	if (loading) {// but with this it does
-		// return <HeaderDesktopView status='loading' />;// doesnt update
-		return <p>Loading...</p>;// updates
-	}
-	if (error) {
-		return <HeaderDesktopView status='error' />
-	}
-	console.log('query completed');
+
 	let categoryNames = data!.categories.map((category: CategoryName) => category.name);
 	return <HeaderDesktopView status='OK' categoryNames={categoryNames} />;
 }
--- src/PureComponents/HeaderDesktopView.tsx
+++ src/PureComponents/HeaderDesktopView.tsx
@@ -1,27 +1,17 @@
 import React, { PureComponent } from 'react';
-import _ from 'lodash';
 
 type Props = { status: 'error' | 'loading' }
            | { status: 'OK', categoryNames: string[] }
 
-type State = Props
-
-class HeaderDesktopView extends PureComponent<Props, State> {
-	constructor(props: Props) {
-		super(props);
-		this.state = _.cloneDeep(props);
-	}
-	
+class HeaderDesktopView extends PureComponent<Props> {
 	render() {
-		console.log(JSON.stringify(this.state));
-		if (this.state.status !== 'OK') {
-			return <div>{this.state.status}</div>;
+		if (this.props.status !== 'OK') {
+			return <div>{this.props.status}</div>;
 		}
-		console.log('rendering actual categories')
 		return (
 			<nav>
 				{
-					this.state.categoryNames.map((name: string) => (
+					this.props.categoryNames.map((name: string) => (
 						<a key={name} href={`\\${name}`}>{` ${name} `}</a>
 					))
 				}
