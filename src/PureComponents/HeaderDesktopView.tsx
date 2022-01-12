08f28308e650bc6b339d9e5d92bb86225f6a3c9f
dazzlemon <daanill13470@gmail.com>
2022-01-12 17:17:09 +0200

    header logo

--- src/PureComponents/HeaderDesktopView.tsx
+++ src/PureComponents/HeaderDesktopView.tsx
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import './HeaderDesktopView.sass';
 import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from './Vector.svg';
 
 type Props = { status: 'error' | 'loading' }
            | { status: 'OK', categories: string[], categoryIndex: number }
@@ -14,18 +15,22 @@ class HeaderDesktopView extends PureComponent<Props> {
 		// it wouldn't believe that status is 'OK' without additional check
 		const categoryIndex = this.props.categoryIndex;
 		return (
-			<nav>
-				{
-					this.props.categories.map((name: string, index: number) => (
-						<Link
-							to={`/${name}`}
-							className={categoryIndex == index ? 'headerElementSelected' : 'headerElement'}
-						>
-							{name}
-						</Link>
-					))
-				}
-			</nav>
+			<div className='header'>
+				<nav className='navigation'>
+					{
+						this.props.categories.map((name: string, index: number) => (
+							<Link
+								to={`/${name}`}
+								className={categoryIndex == index ? 'headerElementSelected' : 'headerElement'}
+							>
+								{name}
+							</Link>
+						))
+					}
+				</nav>
+				<Logo className='logo' />
+				<div className='actions' />
+			</div>
 		);
 	}
 }
