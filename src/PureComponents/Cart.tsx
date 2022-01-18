838e9a60abb74a6ddd17a6b7b66616b49abed896
dazzlemon <daanill13470@gmail.com>
2022-01-18 18:23:06 +0200

    cart is now hiding on outside click

--- src/PureComponents/Cart.tsx
+++ src/PureComponents/Cart.tsx
@@ -1,30 +1,55 @@
-import { useState } from 'react';
 import { ReactComponent as CartIcon } from '../Icons/Cart.svg';
+import React from 'react';
 
-const Cart = () => {
-	const [showOverlay, setShowOverlay] = useState(false);
+type Props = {}
+type State = { isVisible: boolean }
 
-	return (
-		<div className='cart'>
-			<CartIcon className='cartIcon' onClick={() => setShowOverlay(!showOverlay)}/>
-			{showOverlay && (
-				<>
-					<div className='bgEffect'/>
-					<div className='cartOverlay'>
-						<div>
-							Items
+// TODO: extract HOC + better typing
+class Cart extends React.Component<Props, State> {
+	private ref = React.createRef<HTMLDivElement>();
+	constructor(props: Props) {
+		super(props);
+		this.setState({ isVisible: false });
+	} 
+
+	handleClickOutside(e: MouseEvent) {
+    if (this.ref.current && !this.ref.current.contains(e.target as HTMLElement)) {
+      this.setState({ isVisible: false });
+    }
+  };
+
+	componentDidMount() {
+    document.addEventListener('click', this.handleClickOutside, true);
+	}
+
+	componentWillUnmount() {
+		document.removeEventListener('click', this.handleClickOutside, true);
+	}
+
+	render() {
+		return (
+			<div className='cart'>
+				<CartIcon className='cartIcon' onClick={() => this.setState({ isVisible: !this.state.isVisible })}/>
+				{this.state.isVisible && (
+					<>
+						<div className='bgEffect'/>
+						{/* <div className='cartOverlay'> */}
+						<div className='cartOverlay' ref={this.ref}>
+							<div>
+								Items
+							</div>
+							<div>
+								Total
+							</div>
+							<div>
+								Buttons
+							</div>
 						</div>
-						<div>
-							Total
-						</div>
-						<div>
-							Buttons
-						</div>
-					</div>
-				</>
-			)}
-		</div>
-	);
+					</>
+				)}
+			</div>
+		);
+	}
 }
 
 export default Cart;
