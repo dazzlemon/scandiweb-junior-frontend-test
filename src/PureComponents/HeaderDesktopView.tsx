c34e3c7188fe99933cfdd7f09842d6cee0312c33
dazzlemon <daanill13470@gmail.com>
2022-01-18 22:53:58 +0200

    correct overlay

--- src/PureComponents/HeaderDesktopView.tsx
+++ src/PureComponents/HeaderDesktopView.tsx
@@ -17,30 +17,27 @@ class HeaderDesktopView extends PureComponent<Props> {
 	render() {
 		return (
 			<header className='header'>
-				{/* headerContainer is needed for correct work of CartOverlay */}
-				<div className='headerContainer'>
-					<nav className='navigation'>
-						{
-							this.props.categories.map((name: string, index: number) => (
-								<Link
-									to={`/${name}`}
-									className={this.props.categoryIndex == index ? 'headerElementSelected'
-									                                             : 'headerElement'}
-								>
-									{name}
-								</Link>
-							))
-						}
-					</nav>
-					<Logo className='logo' />
-					<div className='actions'>
-						<CurrencySwitcher
-							currencies={this.props.currencies}
-							onChange={this.props.onCurrencyChange}
-							currencyIndex={this.props.currencyIndex}
-						/>
-						<Cart />
-					</div>
+				<nav className='navigation'>
+					{
+						this.props.categories.map((name: string, index: number) => (
+							<Link
+								to={`/${name}`}
+								className={this.props.categoryIndex == index ? 'headerElementSelected'
+																															: 'headerElement'}
+							>
+								{name}
+							</Link>
+						))
+					}
+				</nav>
+				<Logo className='logo' />
+				<div className='actions'>
+					<CurrencySwitcher
+						currencies={this.props.currencies}
+						onChange={this.props.onCurrencyChange}
+						currencyIndex={this.props.currencyIndex}
+					/>
+					<Cart />
 				</div>
 			</header>
 		);
