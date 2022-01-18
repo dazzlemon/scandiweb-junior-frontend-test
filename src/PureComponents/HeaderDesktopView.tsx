1ff5823d844aed35641ae5517e960d3137705be8
dazzlemon <daanill13470@gmail.com>
2022-01-18 22:59:10 +0200

    z-index....

--- src/PureComponents/HeaderDesktopView.tsx
+++ src/PureComponents/HeaderDesktopView.tsx
@@ -17,27 +17,29 @@ class HeaderDesktopView extends PureComponent<Props> {
 	render() {
 		return (
 			<header className='header'>
-				<nav className='navigation'>
-					{
-						this.props.categories.map((name: string, index: number) => (
-							<Link
-								to={`/${name}`}
-								className={this.props.categoryIndex == index ? 'headerElementSelected'
-																															: 'headerElement'}
-							>
-								{name}
-							</Link>
-						))
-					}
-				</nav>
-				<Logo className='logo' />
-				<div className='actions'>
-					<CurrencySwitcher
-						currencies={this.props.currencies}
-						onChange={this.props.onCurrencyChange}
-						currencyIndex={this.props.currencyIndex}
-					/>
-					<Cart />
+				<div className='headerContainer'>
+					<nav className='navigation'>
+						{
+							this.props.categories.map((name: string, index: number) => (
+								<Link
+									to={`/${name}`}
+									className={this.props.categoryIndex == index ? 'headerElementSelected'
+									                                             : 'headerElement'}
+								>
+									{name}
+								</Link>
+							))
+						}
+					</nav>
+					<Logo className='logo' />
+					<div className='actions'>
+						<CurrencySwitcher
+							currencies={this.props.currencies}
+							onChange={this.props.onCurrencyChange}
+							currencyIndex={this.props.currencyIndex}
+						/>
+						<Cart />
+					</div>
 				</div>
 			</header>
 		);
