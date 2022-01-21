ade8d219ce97f595667eb6813e7b36b7bcd3fb8c
dazzlemon <daanill13470@gmail.com>
2022-01-21 15:24:01 +0200

    button style

--- src/Components/ProductView.tsx
+++ src/Components/ProductView.tsx
@@ -108,9 +108,9 @@ const Product = (props: Props) => {
 						</div>
 					);
 				})}
-				<div>Price:</div>
-				<div>{price?.amount} {price?.currency.symbol}</div>
-				<button>Add to cart</button>
+				<div className='priceName'>Price:</div>
+				<div className='price'>{price?.currency.symbol}{price?.amount}</div>
+				<button className='addToCart'>Add to cart</button>
 				<div dangerouslySetInnerHTML={{__html: data!.product.description}} />
 			</div>
 		</div>
