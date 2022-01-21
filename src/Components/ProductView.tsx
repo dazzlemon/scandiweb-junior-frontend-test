753b2009c858d9be35bb61d35f2c739c70ed57e5
dazzlemon <daanill13470@gmail.com>
2022-01-21 15:40:50 +0200

    small responsivness

--- src/Components/ProductView.tsx
+++ src/Components/ProductView.tsx
@@ -87,33 +87,35 @@ const Product = (props: Props) => {
 	const price = data!.product.prices.find(price => price.currency.label == props.currency);
 
 	return (
-		<div className='productView'>
-			<div className='productImagePreviews'>
-				{data!.product.gallery.map(i => <img src={i}/> )}
-			</div>
-			<img src={data!.product.gallery[0] } className='productImage' />
-			<div className='right'>
-				<div className='productName'>{data!.product.name}</div>
-				{data!.product.attributes.map(attr => {
-					return (
-						<div className='attribute'>
-							<div className='name'>{attr.name}: </div>
-							<div className='attributeItems'>
-								{ attr.type == 'text'   ? attr.items.map(i => <div>{i.displayValue}</div> )
-								: attr.type == 'swatch' ? attr.items.map(
-									i => <div className='swatch' style={{backgroundColor: i.value}} />)
-								: null // idk what other types could be?
-								}
+		<main>
+			<div className='productView'>
+				<div className='productImagePreviews'>
+					{data!.product.gallery.map(i => <img src={i}/> )}
+				</div>
+				<img src={data!.product.gallery[0] } className='productImage' />
+				<div className='right'>
+					<div className='productName'>{data!.product.name}</div>
+					{data!.product.attributes.map(attr => {
+						return (
+							<div className='attribute'>
+								<div className='name'>{attr.name}: </div>
+								<div className='attributeItems'>
+									{ attr.type == 'text'   ? attr.items.map(i => <div>{i.displayValue}</div> )
+									: attr.type == 'swatch' ? attr.items.map(
+										i => <div className='swatch' style={{backgroundColor: i.value}} />)
+									: null // idk what other types could be?
+									}
+								</div>
 							</div>
-						</div>
-					);
-				})}
-				<div className='priceName'>Price:</div>
-				<div className='price'>{price?.currency.symbol}{price?.amount}</div>
-				<button className='addToCart'>Add to cart</button>
-				<div className='description' dangerouslySetInnerHTML={{__html: data!.product.description}} />
+						);
+					})}
+					<div className='priceName'>Price:</div>
+					<div className='price'>{price?.currency.symbol}{price?.amount}</div>
+					<button className='addToCart'>Add to cart</button>
+					<div className='description' dangerouslySetInnerHTML={{__html: data!.product.description}} />
+				</div>
 			</div>
-		</div>
+		</main>
 	);
 }
 
