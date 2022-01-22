fd7f3dc4cbce7533110bf35a9ddd76380dc94adf
dazzlemon <daanill13470@gmail.com>
2022-01-22 19:57:26 +0200

    separated ProductContainer from Product(in the same file for now)

--- src/Components/ProductContainer.tsx
+++ src/Components/ProductContainer.tsx
@@ -1,6 +1,6 @@
 import { QueryResult } from '@apollo/client'
 import React from 'react'
-import { Attribute, AttributeSet, Product } from '../Types/ProductContainer'
+import { Attribute, AttributeSet, Product as ProductType } from '../Types/ProductContainer'
 import { PRODUCT } from '../Queries/ProductContainer'
 import './ProductView.sass'
 import { Loading, Error } from '../PureComponents'
@@ -11,10 +11,11 @@ import { Query } from '@apollo/client/react/components'
 type Props = { id: string, currency: string }
 type State = { selected: number[] }
 
-class ProductContainer extends React.Component<Props, State> {
-	constructor(props: Props) {
+type Props_ = { product: ProductType, price: string }
+class Product extends React.Component<Props_, State> {
+	constructor(props: Props_) {
 		super(props)
-		this.state = { selected: [] }
+		this.state = { selected: new Array(props.product.attributes.length).fill(0) }
 	}
 
 	changeAttribute(attrIndex: number, index: number) {
@@ -22,7 +23,7 @@ class ProductContainer extends React.Component<Props, State> {
 		this.setState({selected: [...this.state.selected]})
 	}
 
-	attributeItemProps(i: Attribute, attr: AttributeSet, attrIndex: number, index: number) {
+	attributeItemProps = (i: Attribute, attr: AttributeSet, attrIndex: number, index: number) => {
 		return {
 			className: (attr.type == 'swatch' ? 'swatch' : 'text' ) + 
 				(this.state.selected[attrIndex] == index ? ' selected' : ''),
@@ -30,61 +31,57 @@ class ProductContainer extends React.Component<Props, State> {
 			style: attr.type == 'swatch' ? {backgroundColor: i.displayValue} : undefined,
 			children: attr.type == 'text' ? i.displayValue : ''
 		}
-		return 
 	}
 
-	render() {
-		return (
-			<main>
-				<Query
-					query={PRODUCT}
-					variables={{productId: this.props.id}}
-					onCompleted={(data: { product: Product }) =>
-						this.setState({
-							selected: new Array<number>(data.product.attributes.length ?? 0).fill(0)
-						})
-					}
-				>
-				{(result: QueryResult<{ product: Product }>) => {
-					const { loading, error, data } = result
-					if (loading) return <Loading/>
-					if (error) return <Error/>
-
-					const price = data!.product.prices.find(price => price.currency.label == this.props.currency)
+	render = () => (
+		<div className='productView'>
+			<div className='productImagePreviews'>
+				{this.props.product.gallery.map(i => <img src={i}/> )}
+			</div>
+			<img src={this.props.product.gallery[0] } className='productImage' />
+			<div className='right'>
+				<div className='productName'>{this.props.product.name}</div>
+				{this.props.product.attributes.map((attr, attrIndex) => {
 					return (
-						<div className='productView'>
-							<div className='productImagePreviews'>
-								{data!.product.gallery.map(i => <img src={i}/> )}
-							</div>
-							<img src={data!.product.gallery[0] } className='productImage' />
-							<div className='right'>
-								<div className='productName'>{data!.product.name}</div>
-								{data!.product.attributes.map((attr, attrIndex) => {
-									return (
-										<div className='attribute'>
-											<div className='name'>{attr.name}: </div>
-											<div className='attributeItems'>
-												{
-													attr.items.map((i, index) => (
-														<div {...this.attributeItemProps(i, attr, attrIndex, index)}/>
-													))
-												}
-											</div>
-										</div>
-									)
-								})}
-								<div className='priceName'>Price:</div>
-								<div className='price'>{price?.currency.symbol}{price?.amount}</div>
-								<button className='addToCart'>Add to cart</button>
-								<div className='description' dangerouslySetInnerHTML={{__html: data!.product.description}} />
+						<div className='attribute'>
+							<div className='name'>{attr.name}: </div>
+							<div className='attributeItems'>
+								{
+									attr.items.map((i, index) => (
+										<div {...this.attributeItemProps(i, attr, attrIndex, index)}/>
+									))
+								}
 							</div>
 						</div>
 					)
-				}}
-				</Query>
-			</main>
-		)
-	}
+				})}
+				<div className='priceName'>Price:</div>
+				<div className='price'>{this.props.price}</div>
+				<button className='addToCart'>Add to cart</button>
+				<div className='description' dangerouslySetInnerHTML={{__html: this.props.product.description}} />
+			</div>
+		</div>
+	)
+}
+
+class ProductContainer extends React.Component<Props> {
+	render = () => (
+		<main>
+			<Query
+				query={PRODUCT}
+				variables={{productId: this.props.id}}
+			>
+			{(result: QueryResult<{ product: ProductType }>) => {
+				const { loading, error, data } = result
+				if (loading) return <Loading/>
+				if (error) return <Error/>
+
+				const price = data!.product.prices.find(price => price.currency.label == this.props.currency)
+				return <Product product={data!.product} price={`${price?.currency.symbol}${price?.amount}`} />
+			}}
+			</Query>
+		</main>
+	)
 }
 
 export default ProductContainer
