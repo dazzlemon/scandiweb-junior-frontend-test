a1b190c19562e4044f193c5b1a4ab37036770fdb
dazzlemon <daanill13470@gmail.com>
2022-01-23 18:48:47 +0200

    Attribute refactor

--- src/PureComponents/Product.tsx
+++ src/PureComponents/Product.tsx
@@ -1,5 +1,40 @@
 import React from 'react'
-import { Attribute, AttributeSet, Product as ProductType } from '../Types/ProductContainer'
+import { Attribute as AttributeT, AttributeSet, AttributeType, Product as ProductType } from '../Types/ProductContainer'
+
+type Props_ = {
+	name: string
+	type: AttributeType
+	items: AttributeT[]
+	selectedIndex: number
+	onChange: (index: number) => void
+}
+
+class Attribute extends React.Component<Props_> {
+	attributeItemProps = (i: AttributeT, index: number) => {
+		const swatch = this.props.type == 'swatch'
+		const selected = this.props.selectedIndex == index
+
+		const typeProps = swatch ? { style: {backgroundColor: i.displayValue} }
+		                         : { children: i.displayValue }
+
+		return {
+			className: this.props.type + (selected ? ' selected' : ''),
+			onClick: () => this.props.onChange(index),
+			...typeProps
+		}
+	}
+
+	render = () => (
+		<div className='attribute'>
+			<div className='name'>{this.props.name}: </div>
+			<div className='attributeItems'>
+				{this.props.items.map((i, index) => 
+					<div {...this.attributeItemProps(i, index)}/>
+				)}
+			</div>
+		</div>
+	)
+}
 
 type State = { selected: number[] }
 type Props = { product: ProductType, price: string }
@@ -15,7 +50,7 @@ class Product extends React.Component<Props, State> {
 		this.setState({selected: [...this.state.selected]})
 	}
 
-	attributeItemProps = (i: Attribute, attr: AttributeSet, attrIndex: number, index: number) => {
+	attributeItemProps = (i: AttributeT, attr: AttributeSet, attrIndex: number, index: number) => {
 		return {
 			className: (attr.type == 'swatch' ? 'swatch' : 'text' ) + 
 				(this.state.selected[attrIndex] == index ? ' selected' : ''),
@@ -33,18 +68,15 @@ class Product extends React.Component<Props, State> {
 			<img src={this.props.product.gallery[0] } className='productImage' />
 			<div className='right'>
 				<div className='productName'>{this.props.product.name}</div>
-				{this.props.product.attributes.map((attr, attrIndex) => {
-					return (
-						<div className='attribute'>
-							<div className='name'>{attr.name}: </div>
-							<div className='attributeItems'>
-								{attr.items.map((i, index) => 
-									<div {...this.attributeItemProps(i, attr, attrIndex, index)}/>
-								)}
-							</div>
-						</div>
-					)
-				})}
+				{this.props.product.attributes.map((attr, attrIndex) => 
+					<Attribute
+						name={attr.name}
+						type={attr.type}
+						items={attr.items}
+						selectedIndex={this.state.selected[attrIndex]}
+						onChange={(index) => this.changeAttribute(attrIndex, index)}
+					/>
+				)}
 				<div className='priceName'>Price:</div>
 				<div className='price'>{this.props.price}</div>
 				<button className='addToCart'>Add to cart</button>
