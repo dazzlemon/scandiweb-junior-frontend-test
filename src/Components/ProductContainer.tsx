7223e69aecf24c7fb9c6a06c5ecff0537e22f850
dazzlemon <daanill13470@gmail.com>
2022-01-23 18:21:52 +0200

    Product is in separate file from ProductContainer

--- src/Components/ProductContainer.tsx
+++ src/Components/ProductContainer.tsx
@@ -1,68 +1,14 @@
+import React           from 'react'
+import { Query }       from '@apollo/client/react/components'// TODO: migrate to urql
 import { QueryResult } from '@apollo/client'
-import React from 'react'
-import { Attribute, AttributeSet, Product as ProductType } from '../Types/ProductContainer'
-import { PRODUCT } from '../Queries/ProductContainer'
+
+import { Product as ProductType }  from '../Types/ProductContainer'
+import { PRODUCT }                 from '../Queries/ProductContainer'
+import { Loading, Error, Product } from '../PureComponents'
 import './ProductView.sass'
-import { Loading, Error } from '../PureComponents'
-import { Query } from '@apollo/client/react/components'
 
 // TODO add search params
-
 type Props = { id: string, currency: string }
-type State = { selected: number[] }
-
-type Props_ = { product: ProductType, price: string }
-class Product extends React.Component<Props_, State> {
-	constructor(props: Props_) {
-		super(props)
-		this.state = { selected: new Array(props.product.attributes.length).fill(0) }
-	}
-
-	changeAttribute(attrIndex: number, index: number) {
-		this.state.selected[attrIndex] = index
-		this.setState({selected: [...this.state.selected]})
-	}
-
-	attributeItemProps = (i: Attribute, attr: AttributeSet, attrIndex: number, index: number) => {
-		return {
-			className: (attr.type == 'swatch' ? 'swatch' : 'text' ) + 
-				(this.state.selected[attrIndex] == index ? ' selected' : ''),
-			onClick: () => this.changeAttribute(attrIndex, index),
-			style: attr.type == 'swatch' ? {backgroundColor: i.displayValue} : undefined,
-			children: attr.type == 'text' ? i.displayValue : ''
-		}
-	}
-
-	render = () => (
-		<div className='productView'>
-			<div className='productImagePreviews'>
-				{this.props.product.gallery.map(i => <img src={i}/> )}
-			</div>
-			<img src={this.props.product.gallery[0] } className='productImage' />
-			<div className='right'>
-				<div className='productName'>{this.props.product.name}</div>
-				{this.props.product.attributes.map((attr, attrIndex) => {
-					return (
-						<div className='attribute'>
-							<div className='name'>{attr.name}: </div>
-							<div className='attributeItems'>
-								{
-									attr.items.map((i, index) => (
-										<div {...this.attributeItemProps(i, attr, attrIndex, index)}/>
-									))
-								}
-							</div>
-						</div>
-					)
-				})}
-				<div className='priceName'>Price:</div>
-				<div className='price'>{this.props.price}</div>
-				<button className='addToCart'>Add to cart</button>
-				<div className='description' dangerouslySetInnerHTML={{__html: this.props.product.description}} />
-			</div>
-		</div>
-	)
-}
 
 class ProductContainer extends React.Component<Props> {
 	render = () => (
--- src/PureComponents/Product.tsx
+++ src/PureComponents/Product.tsx
@@ -1,23 +1,57 @@
-// different styling for single image(gallery still can contain single image)
-type Images = { type: 'image', value: string }
-            | { type: 'gallery', value: string[] }
+import React from 'react'
+import { Attribute, AttributeSet, Product as ProductType } from '../Types/ProductContainer'
 
-type Attribute = {
-	type: 'text' | 'swatch',
-	values: string[]
+type State = { selected: number[] }
+type Props = { product: ProductType, price: string }
+
+class Product extends React.Component<Props, State> {
+	constructor(props: Props) {
+		super(props)
+		this.state = { selected: new Array(props.product.attributes.length).fill(0) }
+	}
+
+	changeAttribute(attrIndex: number, index: number) {
+		this.state.selected[attrIndex] = index
+		this.setState({selected: [...this.state.selected]})
+	}
+
+	attributeItemProps = (i: Attribute, attr: AttributeSet, attrIndex: number, index: number) => {
+		return {
+			className: (attr.type == 'swatch' ? 'swatch' : 'text' ) + 
+				(this.state.selected[attrIndex] == index ? ' selected' : ''),
+			onClick: () => this.changeAttribute(attrIndex, index),
+			style: attr.type == 'swatch' ? {backgroundColor: i.displayValue} : undefined,
+			children: attr.type == 'text' ? i.displayValue : ''
+		}
+	}
+
+	render = () => (
+		<div className='productView'>
+			<div className='productImagePreviews'>
+				{this.props.product.gallery.map(i => <img src={i}/> )}
+			</div>
+			<img src={this.props.product.gallery[0] } className='productImage' />
+			<div className='right'>
+				<div className='productName'>{this.props.product.name}</div>
+				{this.props.product.attributes.map((attr, attrIndex) => {
+					return (
+						<div className='attribute'>
+							<div className='name'>{attr.name}: </div>
+							<div className='attributeItems'>
+								{attr.items.map((i, index) => 
+									<div {...this.attributeItemProps(i, attr, attrIndex, index)}/>
+								)}
+							</div>
+						</div>
+					)
+				})}
+				<div className='priceName'>Price:</div>
+				<div className='price'>{this.props.price}</div>
+				<button className='addToCart'>Add to cart</button>
+				<div className='description' dangerouslySetInnerHTML={{__html: this.props.product.description}} />
+			</div>
+		</div>
+	)
 }
 
-type Props = {
-	name: string,
-	images: Images,
-	price: string,
-	attributes: Attribute[]
-	selectedAttributes: number[] // attributes[n].values[selectedAttributes[n]]
-	isAvailable: boolean
-}
-
-const Product = () => {
-
-}
-
-export default Product;
+export default Product
