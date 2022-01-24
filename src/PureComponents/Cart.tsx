3d9a5806b98883926e3a6e205f43c3142d02645f
dazzlemon <daanill13470@gmail.com>
2022-01-24 20:29:08 +0200

    cart is now container that Queries info about cart items

--- src/PureComponents/Cart.tsx
+++ src/PureComponents/Cart.tsx
@@ -2,15 +2,57 @@ import { ReactComponent as CartIcon } from '../Icons/Cart.svg';
 import clickOutside from '../HOCs/clickOutside';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Query } from '@apollo/client/react/components';
+
+import { Product as ProductType }  from '../Types/ProductContainer'
+import { QueryResult } from '@apollo/client';
+import { Loading, Error } from '../PureComponents'
+import { gql } from '@apollo/client';
 
 type Props = { currency: string }
 
 type ProductRecord = {
 	id: string,
-	selectedAttributes: number[]
+	selectedAttributes?: number[]
 }
 
-class CartOverlay extends React.Component<Props> {
+const product = (name: string, id: string) => `
+	${name}: product(id: \"${id}\") {
+		name
+		inStock
+		gallery
+		description
+		category
+		attributes {
+			id
+			name
+			type
+			items {
+				displayValue
+				value
+				id
+			}
+		}
+		prices {
+			currency {
+				label
+				symbol
+			}
+			amount
+		}
+		brand
+	}
+`
+
+const products = (ids: string[]) => {
+	const query = `query GetProducts {
+		${ids.map((i, index) => product('product'+index, i)).join('')}
+	}`
+	console.log('query =', query)
+	return gql(query)
+};
+
+class CartDropdown extends React.Component<Props> {
 	render() {
 		const cart: ProductRecord[] = JSON.parse(localStorage.getItem('cart') ?? '[]');
 		const price = '100.00';
@@ -18,13 +60,46 @@ class CartOverlay extends React.Component<Props> {
 		return (
 			<div className='cartOverlay'>
 				<div className='myBag'>My Bag, <span className='itemCounter'>{cart.length} items</span> </div>
-				<div>
-					{cart.map(product => <div>{product.id}</div>)}
-				</div>
-				<div className='total'>
-					<div>Total</div>
-					<div className='price'>{this.props.currency}{price}</div>
-				</div>
+				<Query
+					query={products(cart.map(({id}) => id))}
+				>
+				{(result: QueryResult<any>) => {//TODO: typing?
+					const { loading, error, data } = result
+					if (loading) return <Loading/>
+					// if bad productId it doesnt return Error but product is undef
+					if (error || !data?.product0) return <Error/>
+	
+					const products = new Array<ProductType>()
+					for (let i = 0; i < cart.length; i++) {
+						products[i] = data[`product${i}`]
+					}
+					return (
+						<>
+							{products.map(product => (
+								<>
+									<div>{product.brand}</div>
+									<div>{product.name}</div>
+									<div>
+										{this.props.currency}
+										{product.prices.find(price => price.currency.symbol == this.props.currency)?.amount}
+									</div>
+									<br/>
+								</>
+							))}
+							<div className='total'>
+								<div>Total</div>
+								<div className='price'>{this.props.currency}{
+									products.map(product =>
+										product.prices.find(price =>
+											price.currency.symbol == this.props.currency
+										)?.amount ?? 0
+									).reduce((a, b) => a + b)
+								}</div>
+							</div>
+						</>
+					)
+				}}
+				</Query>
 				<div className='buttons'>
 					<Link to='/cart' className='viewBag'>View bag</Link>
 					<button className='checkout'>Check out</button>
@@ -34,7 +109,7 @@ class CartOverlay extends React.Component<Props> {
 	}
 }
 
-const Overlay = clickOutside(CartOverlay);
+const Overlay = clickOutside(CartDropdown);
 
 type Props_ = { currency: string }// TODO: probably need to have some shared storage to not redraw?
 type State = { isVisible: boolean }
