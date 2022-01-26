6a17eec379c4628829970e6476c7c5168e32039e
dazzlemon <daanill13470@gmail.com>
2022-01-26 18:08:28 +0200

    cart page tab title

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,9 +1,12 @@
 import PageContainer from '../components/PageContainer'
 
-const Cart = () => (
-	<PageContainer>
-		{() => <div>Cart page</div>}
-	</PageContainer>
-)
+const Cart = () => {
+	document.title = 'Cart'
+	return (
+		<PageContainer>
+			{() => <div>Cart page</div>}
+		</PageContainer>
+	)
+}
 
-export default Cart;
+export default Cart
